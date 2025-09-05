e94a05ad97b956c7ed7dd5c2b5507fd07f19230b
AR04 <90446440+04AR@users.noreply.github.com>
2025-09-05 18:56:50 +0530

    Update connection.go

--- internal/server/connection.go
+++ internal/server/connection.go
@@ -2,31 +2,49 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"time"
+
+	"go-server/internal/db"
 
 	"github.com/coder/websocket"
 )
 
-type Connection struct {
-	hub    *Hub
-	conn   *websocket.Conn
-	sendCh chan []byte
+// Message represents a WebSocket message with type, sender ID, channel, and content.
+type ClientMessage struct {
+	ID     string        `json:"id"`
+	Type   string        `json:"type"`
+	Action string        `json:"action"`
+	Args   []interface{} `json:"args"`
 }
 
-func NewConnection(hub *Hub, conn *websocket.Conn) *Connection {
+type ServerResponse struct {
+	ID     string      `json:"id"`
+	Type   string      `json:"type"` // "response"
+	Status string      `json:"status"`
+	Result interface{} `json:"result,omitempty"`
+	Error  string      `json:"error,omitempty"`
+}
+
+type Connection struct {
+	rm     *db.RedisManager
+	conn   *websocket.Conn
+	SendCh chan []byte
+}
+
+func NewConnection(rm *db.RedisManager, conn *websocket.Conn) *Connection {
 	c := &Connection{
-		hub:    hub,
+		rm:     rm,
 		conn:   conn,
-		sendCh: make(chan []byte, 16),
+		SendCh: make(chan []byte, 16),
 	}
-	hub.register <- c
 	return c
 }
 
 func (c *Connection) ReadPump(ctx context.Context) {
 	defer func() {
-		c.hub.unregister <- c
 		c.conn.Close(websocket.StatusNormalClosure, "closing")
 	}()
 
@@ -36,15 +54,34 @@ func (c *Connection) ReadPump(ctx context.Context) {
 			log.Println("read error:", err)
 			break
 		}
-		// Broadcast incoming message to all
-		c.hub.broadcast <- msg
+
+		// Parse incoming message
+		var packet ClientMessage
+		if err := json.Unmarshal(msg, &packet); err != nil {
+			log.Println("json unmarshal error:", err)
+			continue
+		}
+
+		// Dispatch based on action
+		switch packet.Action {
+		case "ping":
+			c.sendResponse(packet.ID, map[string]string{"message": "pong"})
+		default:
+			// Try to call Lua script
+			res, err := c.rm.CallScript(ctx, packet.Action, nil, packet.Args...)
+			if err != nil {
+				c.sendError(packet.ID, "script_error", err.Error())
+			} else {
+				c.sendResponse(packet.ID, res)
+			}
+		}
 	}
 }
 
 func (c *Connection) WritePump(ctx context.Context) {
 	defer c.conn.Close(websocket.StatusNormalClosure, "writer closing")
 
-	for msg := range c.sendCh {
+	for msg := range c.SendCh {
 		writeCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
 		err := c.conn.Write(writeCtx, websocket.MessageText, msg)
 		cancel()
@@ -53,4 +90,26 @@ func (c *Connection) WritePump(ctx context.Context) {
 			break
 		}
 	}
+}
+
+func (c *Connection) sendResponse(id string, result interface{}) {
+	resp := ServerResponse{
+		ID:     id,
+		Type:   "response",
+		Status: "ok",
+		Result: result,
+	}
+	data, _ := json.Marshal(resp)
+	c.SendCh <- data
+}
+
+func (c *Connection) sendError(id, code, msg string) {
+	resp := ServerResponse{
+		ID:     id,
+		Type:   "response",
+		Status: "error",
+		Error:  fmt.Sprintf("%s: %s", code, msg),
+	}
+	data, _ := json.Marshal(resp)
+	c.SendCh <- data
 }
