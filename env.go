ea038fdb3f1442e6a5666e987d1793fa7cd2d7e6
AR04 <90446440+04AR@users.noreply.github.com>
2025-09-13 22:30:12 +0530

    Update env.go

--- env.go
+++ env.go
@@ -5,38 +5,29 @@ import (
 )
 
 const (
-	sqlDb              = "./users.db"
-	dsn                = "postgres://username:password@localhost:5432/mydb" // PostgressDB credentials
 	wsAddr             = ":8080"
 	profilerAddr       = ":9090"
-	redisAddr          = ":6379"                // Assumes running in Docker Compose network
-	redisPassword      = ""                     // No password set
-	redisLuaScriptPath = "./lua_scripts"        // Directory with Lua scripts
-	jwtSecretKey       = "your-very-secret-key" // CHANGE THIS in production
+	redisAddr          = ":6379"         // Assumes running in Docker Compose network
+	redisPassword      = ""              // No password set
+	redisLuaScriptPath = "./lua_scripts" // Directory with Lua scripts
 )
 
 // Env holds all application-wide environment values.
 var (
-	SQLDBPath          string
-	PostgresDSN        string
 	WSAddr             string
 	ProfilerAddr       string
 	RedisAddr          string
 	RedisPassword      string
 	RedisLuaScriptPath string
-	JWTSecretKey       string
 )
 
 // LoadEnv reads env vars with defaults for all required configs.
 func init() {
-	SQLDBPath = getEnv("APP_SQLITE_PATH", sqlDb)
-	PostgresDSN = getEnv("APP_POSTGRES_DSN", dsn)
 	WSAddr = getEnv("APP_WS_ADDR", wsAddr)
 	ProfilerAddr = getEnv("APP_PROFILER_ADDR", profilerAddr)
 	RedisAddr = getEnv("APP_REDIS_ADDR", redisAddr)
 	RedisPassword = getEnv("APP_REDIS_PASSWORD", redisPassword)
 	RedisLuaScriptPath = getEnv("APP_REDIS_LUA_SCRIPT_Path", redisLuaScriptPath)
-	JWTSecretKey = getEnv("APP_JWT_SECRET", jwtSecretKey)
 }
 
 // Helper: read env or fallback
