30560b97d5162466c031988d21edd8e6a8cd0c0d
AR04 <90446440+04AR@users.noreply.github.com>
2025-10-04 23:35:12 +0530

    Add files via upload

---
+++ lua_scripts/addData.lua
@@ -0,0 +1,17 @@
+local hash   = tostring(KEYS[1])
+local action = tostring(ARGV[1])
+local field  = tostring(ARGV[2])
+
+if action == "add" then
+    local value = tostring(ARGV[3] or "")
+    redis.call("HSET", hash, field, value)
+    -- Return a JSON string
+    return cjson.encode({status="ok", action="add", field=field, value=value})
+
+elseif action == "remove" then
+    redis.call("HDEL", hash, field)
+    return cjson.encode({status="ok", action="remove", field=field})
+
+else
+    return cjson.encode({status="error", message="invalid action: "..action})
+end
