9d5c429a2f7fb50a3d7d83e0c3f1afeb2ca29b02
AR04 <90446440+04AR@users.noreply.github.com>
2025-10-07 23:41:21 +0530

    Update addData.lua

--- lua_scripts/addData.lua
+++ lua_scripts/addData.lua
@@ -2,6 +2,8 @@ local hash   = tostring(KEYS[1])
 local action = tostring(ARGV[1])
 local field  = tostring(ARGV[2])
 
+redis.call("PUBLISH", "room1", cjson.encode({action=action, field=field, hash=hash}))
+
 if action == "add" then
     local value = tostring(ARGV[3] or "")
     redis.call("HSET", hash, field, value)
@@ -15,3 +17,4 @@ elseif action == "remove" then
 else
     return cjson.encode({status="error", message="invalid action: "..action})
 end
+
